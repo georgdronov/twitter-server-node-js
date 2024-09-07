3c5d08050f1939504be03e2f7584647549e52661
Georg Dronov <dronovgeorg72@gmail.com>
2024-09-07 16:24:54 +0300

    add status and env

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 import express from "express";
+import { twitRouter } from "./src/twit/twit.controller.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 async function main() {
   app.use(express.json());
 
-  app.use("/api/twit", (req, res) => {
-    res
-      .json({
-        message: "success",
-      })
-      .status(200);
+  app.use("/api/twits", twitRouter);
+
+  app.all("*", (req, res) => {
+    res.status(404).json({ message: "Not Found" });
   });
 
-  app.listen(4200, () => {
+  app.listen(process.env.PORT || 4200, () => {
     console.log("Server is running on port 4200");
   });
 }
---
+++ src/twit/twit.controller.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { TwitService } from "./twit.service.js";
+
+const router = Router();
+
+const twitService = new TwitService();
+
+router.post("/", (req, res) => {
+  if (!req.body?.text?.length) {
+    return res.status(400).json({ message: "Text is required" });
+  }
+
+  const twit = twitService.createTwit(req.body);
+  res.status(201).json(twit);
+});
+
+export const twitRouter = router;
