d34b16c5fff2d0495c70506a5a850aaffe7219bd
Georg Dronov <dronovgeorg72@gmail.com>
2024-09-07 15:05:09 +0300

    create first response

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+async function main() {
+  app.use(express.json());
+
+  app.use("/api/twit", (req, res) => {
+    res
+      .json({
+        message: "success",
+      })
+      .status(200);
+  });
+
+  app.listen(4200, () => {
+    console.log("Server is running on port 4200");
+  });
+}
+
+main();
